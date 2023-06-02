1957217a41259afc28de36ee8fc51d777bcc76c7
米川貴也 <zuomujianwu1@gmail.com>
2023-06-02 21:19:22 +0900

    初回コミット

---
+++ ruby ri20min.rb
@@ -0,0 +1,53 @@
+class MegaGreeter
+    attr_accessor :names
+
+    def initialize(names =  "World")
+        @names = names
+    end
+
+    def say_hi
+        if @names.nil?
+            puts "..."
+        elsif @names.respond_to?("each")
+            @names.each do |name|
+                puts "Hello #{name}!"
+            end
+        else
+            put "Hello #{@names}!"
+        end
+    end
+
+    def say_bye
+        if @names.nil?
+            puts "..."
+        elsif @names.respond_to("join")
+            puts "Goodbye #{@names.join(", ")}. Come back soon!"
+        else
+            puts "Goodbye #{@name}. Come back soon!"
+        end
+    end
+end
+
+
+if__FILE__ == $0
+mg = MegaGreeter.new
+mg.say_hi
+mg.say_bye
+
+
+  # Change name to be "Zeke"
+  mg.names = "Zeke"
+  mg.say_hi
+  mg.say_bye
+
+  # Change the name to an array of names
+  mg.names = ["Albert", "Brenda", "Charles",
+              "Dave", "Engelbert"]
+  mg.say_hi
+  mg.say_bye
+
+  # Change to nil
+  mg.names = nil
+  mg.say_hi
+  mg.say_bye
+end
