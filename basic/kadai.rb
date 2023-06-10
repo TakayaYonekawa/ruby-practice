80a01c833ccc00d8d1e07f9b66432e4a2c06f076
米川貴也 <zuomujianwu1@gmail.com>
2023-06-10 22:31:11 +0900

    課題

--- basic/kadai.rb
+++ basic/kadai.rb
@@ -1,11 +1,9 @@
-age = 10
-
-if age >= 10 && age < 20
-    p "10代"
-elsif age >= 20 && age < 30
-    p "20代"
-elsif age >= 30 && age < 40
-    p "30代"
-else 
-    p "それ以外"
+for i in 0 .. 10
+    if i == 3
+        next
+    elsif i == 7
+        break
+    else
+        p i
+    end
 end
--- basic/lesson11_01.rb
+++ basic/lesson11_01.rb
@@ -1,5 +1,8 @@
-for i in 0 .. 4
-    for j in 0 .. 2
-        p i.to_s + "-" + j.to_s
-    end
+arr = [2,4,6,8,10]
+sum = 0
+
+for i in arr 
+    sum += i
 end
+
+p sum
