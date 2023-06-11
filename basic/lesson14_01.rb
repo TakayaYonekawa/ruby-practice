5c4dbba5931e5d886bcd4db7b5c4fd7873fb3d92
米川貴也 <zuomujianwu1@gmail.com>
2023-06-11 22:06:50 +0900

    テスト

--- basic/lesson14_01.rb
+++ basic/lesson14_01.rb
@@ -25,3 +25,11 @@ class Student
     attr_accessor :name
 end
 
+
+a001 = Student.new("sato")
+data = [70,65,50,90,30]
+avg = a001.cal_avg(data)
+result = a001.jedge(avg)
+p a001.name
+p avg
+p result
